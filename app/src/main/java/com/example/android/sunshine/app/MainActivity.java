09b4e36b643c9c66a697926b1965662d996ce1cd
Jenny Olsen <jenny.a.olsen@gmail.com>
2016-02-17 10:40:16 -0800

    add starter code for lesson 4

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -89,7 +89,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onStart() {
         super.onStart();
-
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
